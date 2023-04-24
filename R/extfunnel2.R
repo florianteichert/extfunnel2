fe117596fd2b0a342cadd6f8065e82dd6ea07130
Florian Teichert <teichert.florian@gmail.com>
2023-04-25 00:14:03 +0200

    0.0.0.9000

---
+++ R/extfunnel2.R
@@ -0,0 +1,203 @@
+#' Generate Extended Funnel Plot
+#'
+#' Inspired by the archived extfunnel package developed by Dean Langan et al. The function generates an extended funnel plot with shaded contours that show the impact of a new study with a certain effect estimate and standard error (or sample size) on the conclusions of an updated meta-analysis. Uses ggplot2 instead of base R, allows specification of the meta-analytic model as well as simulation using sample size per group.
+#'
+#' @param yi A numeric vector with effect estimates for each study. Odds/ risk ratios should be log-transformed.
+#' @param sei A numeric vector with standard errors for each study. Specify either 'sei' of 'sd' AND 'n'. If 'sei' is specified the y-axis will correspond to the standard error,
+#' @param sd A single numeric value corresponding to the assumed standard deviation per group of a future study. Specify either 'sei' or 'sd' AND 'n'. If sd and n are specified the y-axis will correspond to the sample size per group.
+#' @param n A numeric vector of the average sample size per group for each study. Only needed when 'sd' is specified.
+#' @param swe A single numeric value corresponding to the smallest worthwhile effect. Odds/ risk ratios should be log-transformed.
+#' @param method A character string indicating which method should be used to estimate tau. Default is "REML". See \link[metafor]{rma} of the metafor package for more options.
+#' @param test A character string indicating which method should be use to calculate confidence intervals. Default is "z". \link[metafor]{rma} of the metafor package for more options.
+#' @param contour_points A numeric value indicating the number of contour points to be used for simulation. Default is 50. For high quality contours 500 is recommend (this might take while!).
+#' @param x_lim A numeric vector with two values indicating the lower and upper limits of the simulated effect estimates (corresponds to x-axis limits). Odds/ risk ratios should be log-transformed.
+#' @param y_lim A numeric vector with two values indicating the lower and upper limits of the simulated standard errors (corresponds to y-axis limits). If 'sd' and 'n' are provided 'y_lim' corresponds to lower and upper limits of the sample size per group.
+#' @param x_lab A character string indicating the x-axis label for the plot. Default is "Effect size".
+#' @param y_lab A character string indicating the y-axis label for the plot. Default is "Standard error".
+#' @param x_ticks A numeric vector indicating the tick marks for the x-axis. Default is NULL. Odds/ risk ratios should be log-transformed.
+#' @param y_ticks A numeric vector indicating the tick marks for the y-axis. Default is NULL.
+#' @param legend_pos A character string indicating the position of the legend in the plot. Default is "none". Can be "top", "right", "bottom", or "left".
+#' @param exp A logical value indicating whether the x-axis should be on a logarithmic scale (in case odds/ risk ratios are provided). Default is FALSE.
+#'
+#' @import dplyr ggplot2 tibble metafor furrr
+#'
+#' @examples
+#' # Load package once installed
+#' library(extfunnel2)
+#'
+#' # Reproducing the extended funnel plot from Ferreira et al. 2012
+#' # http://dx.doi.org/10.1136/bmj.e5913
+#' # Random effects meta-analysis (effect of exercise on chronic low back pain)
+#' data <- data.frame(yi = c(-10.0, -16.3, -27.0, -14.0, -15.1, -2.1, -19.0, -8.7),
+#'                    sei = c(3.4, 5.8, 3.4, 5.3, 2.9, 1.1, 7.2, 4.2))
+#'
+#' # Generate extended funnel plot
+#' extfunnel2(data$yi, data$sei,
+#'            swe = -20,
+#'            method = "DL",
+#'            contour_points = 50,
+#'            x_lim = c(-40, 10), y_lim = c(0, 15),
+#'            x_ticks = seq(from = -40, to = 10, by = 10),
+#'            y_ticks = seq(from = 0, to = 15, by = 3),
+#'            x_lab = "Effect on pain (100 point scale)")
+#'
+#'@seealso
+#'\code{\link{https://doi.org/10.1016/j.jclinepi.2011.10.009}}
+#'\code{\link{https://doi.org/10.1136/bmj.e5913}}
+#'
+#' @return An extended funnel plot (ggplot2 object) with shaded contours that show the impact of a new study with a certain effect estimate and standard error (or sample size) on the conclusions of an updated meta-analysis.
+#' @export
+extfunnel2 <- function(yi, sei, sd = NULL, n = NULL, swe,
+                       method = "REML", test = "z",
+                       contour_points = 50,
+                       x_lim, y_lim,
+                       x_lab = "Effect size", y_lab = "Standard error",
+                       x_ticks = NULL, y_ticks = NULL,
+                       legend_pos = "none", exp = FALSE) {
+
+  if (!is.null(sei) & !is.null(sd) & !is.null(n)) {
+    message("Note: 'sei' will be ignored since 'sd' and 'n' are specified.")
+    message("Note: Only works for mean differences.")
+  }
+
+  if (!is.null(sd) & !is.null(n)) {
+    extfunnel2_n(yi, sei, method = method, test = test, contour_points = contour_points, swe = swe,
+                 x_lim = x_lim, y_lim = y_lim, sd = sd, n = n, x_lab = xlab, y_lab = ylab, x_ticks =
+                   x_ticks, y_ticks = y_ticks, legend_pos = legend_pos)
+  } else{
+  # Run meta-analysis with current studies
+  res <- tidy_meta(rma.uni(yi = yi, sei = sei, method = method, test = test))
+
+
+  ### Simulation
+
+  # Create n = contour_points values from the lower to upper limit of the effect estimates (yi_sim) and standard errors (sei_sim)
+  yi_sim <- seq(x_lim[1], x_lim[2], length.out = contour_points)
+  sei_sim <- seq(y_lim[1], y_lim[2], length.out = contour_points)
+
+  # Standard errors cannot be 0
+  sei_sim[sei_sim <= 0] <- 0.0000001
+
+  # Create all possible combinations of effect estimates and standard errors to map out the whole plot area
+  df_sim <- expand_grid(yi_sim = yi_sim, sei_sim = sei_sim) %>%
+    mutate(id = row_number()) %>%
+    select(id, everything())
+
+  # Use parallel processing for faster computation
+  plan(multisession)
+
+  # Combine every simulated effect estimate and standard error combination with the original effect estimates and standard errors
+  df_comb <-
+    future_pmap(df_sim, function(id, yi_sim, sei_sim) {
+      tibble(
+        id = id,
+        yi_sim = c(yi, yi_sim),
+        sei_sim = c(sei, sei_sim)
+      )
+    }) %>%
+    list_rbind()
+
+  plan(sequential)
+
+  res_sim <- df_comb %>%
+    group_by(id) %>%
+    group_modify(
+      .data = .,
+      # Rerun meta-analysis for every combination of old studies + simulated new study
+      .f = ~ suppressWarnings(tidy_meta(
+        rma.uni(yi = .$yi_sim, sei = .$sei_sim, method = method, test = test, control = list(tau2.max = 10000))
+      ))
+    ) %>%
+    ungroup() %>%
+    # Compare confidence intervals of pooled effect estimates with the smallest worthwhile effect (swe)
+    mutate(sig = case_when(
+      ci.lb < swe & ci.ub < swe ~ "Clearly worthwhile",
+      ci.lb < swe & ci.ub > swe ~ "Unclear if worthwhile",
+      ci.lb > swe & ci.ub > swe ~ "Clearly not worthwhile"
+    )) %>%
+    select(ci.lb, ci.ub, sig) %>%
+    mutate(sig = as.factor(sig)) %>%
+    bind_cols(yi_sim = df_sim$yi_sim, sei_sim = df_sim$sei_sim, .)
+
+
+  ### Create the extended funnel plot
+
+  # Calculate y axis length
+  y_axis_l <- y_lim[2] - y_lim[1]
+
+  if (exp == FALSE) {
+    plot <- ggplot(data = res_sim) +
+      # Plot one rectangle for each simulated meta-analytic result (color depends on sig, see case_when above)
+      geom_tile(aes(x = yi_sim, y = sei_sim, fill = sig)) +
+      geom_hline(yintercept = y_ticks, color = "white") +
+      # Add vertical line corresponding to the smallest worthwhile effect (swe)
+      geom_vline(aes(xintercept = swe, color = "Smallest worthwhile effect"), linetype = "dashed") +
+      # Add vertical line corresponding to the pooled estimate of the currently existing studies
+      geom_vline(aes(xintercept = res$est, color = "Current pooled estimate"), linetype = "solid") +
+      # Add points corresponding to point estimates of currently existing studies
+      geom_point(
+        data = data.frame(x = yi, y = sei),
+        aes(x = x, y = y, shape = "Current point estimates"),
+        size = 2.5, stroke = 0.7,
+        fill = "#42727D",
+        color = "#070F14"
+      ) +
+      labs(x = x_lab, y = y_lab) +
+      scale_color_manual(values = c("Smallest worthwhile effect" = "black", "Current pooled estimate" = "black")) +
+      scale_shape_manual(values = c("Current point estimates" = 21)) +
+      scale_fill_manual(values = c("Clearly worthwhile" = "#bf32de", "Unclear if worthwhile" = "#f7766d", "Clearly not worthwhile" = "#03bfc4")) +
+      theme_classic() +
+      theme(legend.title = element_blank()) +
+      guides(color = guide_legend(
+        override.aes = list(
+          linetype = c("solid", "dashed")
+        )
+      ))
+
+    plot <- axis_limits(plot, x_lim, y_lim, x_ticks, y_ticks)
+    # Back transform log scale
+  } else {
+    plot <- ggplot(data = res_sim) +
+      # Plot one rectangle for each simulated meta-analytic result (color depends on sig, see case_when above)
+      geom_tile(aes(x = exp(yi_sim), y = sei_sim, fill = sig)) +
+      geom_hline(yintercept = y_ticks, color = "white") +
+      # Add vertical line corresponding to the smallest worthwhile effect (swe)
+      geom_vline(aes(xintercept = exp(swe), color = "Smallest worthwhile effect"), linetype = "dashed") +
+      # Add vertical line corresponding to the pooled estimate of the currently existing studies
+      geom_vline(aes(xintercept = exp(res$est), color = "Current pooled estimate"), linetype = "solid") +
+      # Add points corresponding to point estimates of currently existing studies
+      geom_point(
+        data = data.frame(x = exp(yi), y = sei),
+        aes(x = x, y = y, shape = "Current point estimates"),
+        size = 2.5, stroke = 0.7,
+        fill = "#42727D",
+        color = "#070F14"
+      ) +
+      labs(x = x_lab, y = y_lab) +
+      scale_color_manual(values = c("Smallest worthwhile effect" = "black", "Current pooled estimate" = "black")) +
+      scale_shape_manual(values = c("Current point estimates" = 21)) +
+      scale_fill_manual(values = c("Clearly worthwhile" = "#ded032", "Unclear if worthwhile" = "#f7766d", "Clearly not worthwhile" = "#03bfc4")) +
+      theme_classic() +
+      theme(legend.title = element_blank()) +
+      guides(color = guide_legend(
+        override.aes = list(
+          linetype = c("solid", "dashed")
+        )
+      ))
+
+    plot <- axis_limits_exp(plot, x_lim, y_lim, x_ticks, y_ticks)
+  }
+
+  plot +
+    theme(
+      axis.line = element_blank(),
+      axis.ticks = element_line(color = "white"),
+      axis.text = element_text(margin = margin(t = 0, r = 0, b = 10, l = 0), size = 10, color = "black"),
+      axis.ticks.length = unit(-0.12, "cm"),
+      axis.title = element_text(size = 10, face = "bold"),
+      axis.title.x = element_text(hjust = 1, margin = margin(t = 10)),
+      axis.title.y = element_text(hjust = 1, margin = margin(r = 12)),
+      legend.position = legend_pos
+    )
+  }
+}
---
+++ R/helper_functions.R
@@ -0,0 +1,170 @@
+tidy_meta <- function(meta_object) {
+  pred_vals <- predict(meta_object)
+
+  tibble(
+    est = pred_vals$pred,
+    se = pred_vals$se,
+    ci.lb = pred_vals$ci.lb,
+    ci.ub = pred_vals$ci.ub,
+    pi.lb = pred_vals$pi.lb,
+    pi.ub = pred_vals$pi.ub,
+  )
+}
+
+axis_limits <- function(plot, x_lim, y_lim, x_ticks, y_ticks) {
+  if (!is_null(x_ticks) & !is_null(y_ticks)) {
+    plot <- plot +
+      scale_x_continuous(breaks = x_ticks, expand = expansion(mult = 0)) +
+      scale_y_reverse(breaks = y_ticks, expand = expansion(mult = 0)) +
+      coord_cartesian(ylim = c(y_lim[2], y_lim[1]), xlim = c(x_lim[1], x_lim[2]))
+  } else {
+    plot <- plot +
+      scale_x_continuous(expand = expansion(mult = 0)) +
+      scale_y_reverse(expand = expansion(mult = 0)) +
+      coord_cartesian(ylim = c(y_lim[2], y_lim[1]), xlim = c(x_lim[1], x_lim[2]))
+  }
+  plot
+}
+
+axis_limits_exp <- function(plot, x_lim, y_lim, x_ticks, y_ticks) {
+  if (!is_null(x_ticks) & !is_null(y_ticks)) {
+    plot <- plot +
+      scale_x_log10(breaks = exp(x_ticks), expand = expansion(mult = 0)) +
+      scale_y_reverse(breaks = y_ticks, expand = expansion(mult = 0)) +
+      coord_cartesian(ylim = c(y_lim[2], y_lim[1]), xlim = c(exp(x_lim[1]), exp(x_lim[2])))
+  } else {
+    plot <- plot +
+      scale_x_log10(expand = expansion(mult = 0)) +
+      scale_y_reverse(expand = expansion(mult = 0)) +
+      coord_cartesian(ylim = c(y_lim[2], y_lim[1]), xlim = c(exp(x_lim[1]), exp(x_lim[2])))
+  }
+  plot
+}
+
+axis_limits_n <- function(plot, x_lim, y_lim, x_ticks, y_ticks) {
+  if (!is_null(x_ticks) & !is_null(y_ticks)) {
+    plot <- plot +
+      scale_x_continuous(breaks = x_ticks, expand = expansion(mult = 0)) +
+      scale_y_continuous(breaks = y_ticks, expand = expansion(mult = 0)) +
+      coord_cartesian(ylim = c(y_lim[1], y_lim[2]), xlim = c(x_lim[1], x_lim[2]))
+  } else {
+    plot <- plot +
+      scale_x_continuous(expand = expansion(mult = 0)) +
+      scale_y_continuous(expand = expansion(mult = 0)) +
+      coord_cartesian(ylim = c(y_lim[1], y_lim[2]), xlim = c(x_lim[1], x_lim[2]))
+  }
+  plot
+}
+
+extfunnel2_n <- function(yi, sei, method = "REML", test = "z",
+                         contour_points = 50, swe = 0,
+                         x_lim, y_lim,
+                         sd, n,
+                         x_lab = "Effect size", y_lab = "Sample size per group",
+                         x_ticks = NULL, y_ticks = NULL,
+                         legend_pos = "right") {
+  # Run meta-analysis with current studies
+  res <- tidy_meta(rma.uni(yi = yi, sei = sei, method = method, test = test))
+
+
+  ### Simulation
+
+  # Create n = contour_points values from the lower to upper limit of the effect estimates (yi_sim) and sample sizes (n_sim)
+  yi_sim <- seq(x_lim[1], x_lim[2], length.out = contour_points)
+  n_sim <- seq(y_lim[1], y_lim[2], length.out = contour_points)
+
+  # Minimal sample size per arm = 10
+  n_sim[n_sim <= 10] <- 10
+
+  # Create all possible combinations of effect estimates and sample sizes to map out the whole plot area
+  df_sim <- expand_grid(yi_sim = yi_sim, n_sim = n_sim) %>%
+    mutate(id = row_number()) %>%
+    # Calculate standard errors, equal group sizes and fixed standard deviation are assumed
+    mutate(n1i = n_sim, n1i = n_sim, m1i = 0, sd1i = sd, n2i = n_sim, m2i = 0 - yi_sim, sd2i = sd) %>%
+    escalc(n1i = n1i, n2i = n2i, m1i = m1i, m2i = m2i, sd1i = sd1i, sd2i = sd2i, data = ., measure = "MD") %>%
+    tibble() %>%
+    mutate(sei_sim = sqrt(vi)) %>%
+    select(id, yi_sim, n_sim, sei_sim)
+
+  # Use parallel processing for faster computation
+  plan(multisession)
+
+  # Combine every simulated effect estimate and standard error combination with the original effect estimates and standard errors
+  df_comb <-
+    future_pmap(df_sim, function(id, yi_sim, n_sim, sei_sim) {
+      tibble(
+        id = id,
+        yi_sim = c(yi, yi_sim),
+        n_sim = n_sim,
+        sei_sim = c(sei, sei_sim)
+      )
+    }) %>%
+    list_rbind()
+
+  res_sim <- df_comb %>%
+    group_by(id) %>%
+    group_modify(
+      .data = .,
+      # Rerun meta-analysis for every combination of old studies + simulated new studies
+      .f = ~ suppressWarnings(tidy_meta(
+        rma.uni(yi = .$yi_sim, sei = .$sei_sim, method = "DL", test = "z", control = list(tau2.max = 10000))
+      ))
+    ) %>%
+    ungroup() %>%
+    # Compare confidence intervals of pooled effect estimates with the smallest worthwhile effect (swe)
+    mutate(sig = case_when(
+      ci.lb < swe & ci.ub < swe ~ "Clearly worthwhile",
+      ci.lb < swe & ci.ub > swe ~ "Unclear if worthwhile",
+      ci.lb > swe & ci.ub > swe ~ "Clearly not worthwhile"
+    )) %>%
+    select(ci.lb, ci.ub, sig) %>%
+    mutate(sig = as.factor(sig)) %>%
+    bind_cols(yi_sim = df_sim$yi_sim, n_sim = df_sim$n_sim, sei_sim = df_sim$sei_sim, .)
+
+  ### Create the extended funnel plot
+
+  # Calculate y axis length
+  y_axis_l <- y_lim[2] - y_lim[1]
+
+  plot <- ggplot(data = res_sim) +
+    # Plot one rectangle for each simulated meta-analytic result (color depends on sig, see case_when above)
+    geom_tile(aes(x = yi_sim, y = n_sim, fill = sig)) +
+    geom_hline(yintercept = y_ticks, color = "white") +
+    # Add vertical line corresponding to the smallest worthwhile effect (swe)
+    geom_vline(aes(xintercept = swe, color = "Smallest worthwhile effect"), linetype = "dashed") +
+    # Add vertical line corresponding to the pooled estimate of the currently existing studies
+    geom_vline(aes(xintercept = res$est, color = "Current pooled estimate"), linetype = "solid") +
+    # Add points corresponding to point estimates of currently existing studies
+    geom_point(
+      data = data.frame(x = yi, y = n),
+      aes(x = x, y = y, shape = "Current point estimates"),
+      size = 2.5, stroke = 0.7,
+      fill = "#42727D",
+      color = "#070F14"
+    ) +
+    labs(x = x_lab, y = y_lab) +
+    scale_color_manual(values = c("Smallest worthwhile effect" = "black", "Current pooled estimate" = "black")) +
+    scale_shape_manual(values = c("Current point estimates" = 21)) +
+    scale_fill_manual(values = c("Clearly worthwhile" = "#bf32de", "Unclear if worthwhile" = "#f7766d", "Clearly not worthwhile" = "#03bfc4")) +
+    theme_classic() +
+    theme(legend.title = element_blank()) +
+    guides(color = guide_legend(
+      override.aes = list(
+        linetype = c("solid", "dashed")
+      )
+    ))
+
+  plot <- axis_limits_n(plot, x_lim, y_lim, x_ticks, y_ticks)
+
+  plot +
+    theme(
+      axis.line = element_blank(),
+      axis.ticks = element_line(color = "white"),
+      axis.text = element_text(margin = margin(t = 0, r = 0, b = 10, l = 0), size = 10, color = "black"),
+      axis.ticks.length = unit(-0.12, "cm"),
+      axis.title = element_text(size = 10, face = "bold"),
+      axis.title.x = element_text(hjust = 1, margin = margin(t = 10)),
+      axis.title.y = element_text(hjust = 1, margin = margin(r = 12)),
+      legend.position = legend_pos
+    )
+}
